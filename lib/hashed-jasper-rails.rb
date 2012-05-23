6250c1901eb077edd772f71a58d1c2fa36300ae5
Kazuhisa Yamamoto <ak.hisashi@gmail.com>
2012-05-23 17:02:00 +0900

    add jar.

---
+++ lib/hashed-jasper-rails.rb
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+require 'hashed-jasper-rails/jasper-rails'
+require 'hashed-jasper-rails/jasper_source_builder'
---
+++ lib/hashed-jasper-rails/jasper_source_builder.rb
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+require "rexml/document"
+
+class JasperSourceBuilder
+  def initialize(data_source, model = "jasper", record = "record")
+    @out_doc = REXML::Document.new()
+    @out_doc.add(REXML::XMLDecl.new(version="1.0", encoding="UTF-8"))
+    elem = REXML::Element.new(model)
+    elem.add_attribute("type","array")
+
+    data_source.each do |data|
+      row = REXML::Element.new(record)
+      data.each_pair do |k,v|
+        cell = REXML::Element.new(k.to_s)
+        cell.add_text(v.to_s)
+        row.add_element(cell)
+      end
+      elem.add_element(row)
+    end
+    @out_doc.add(elem)
+  end
+
+  def to_xml(dummy)
+    @out_doc.to_s
+  end
+end
+
+class ApplicationController
+  def jasper_pdf(arg)
+    model_name = arg[:model]
+    record_name = arg[:record]
+    if arg[:template].present?
+      jasper_file = File.join("app/views",arg[:template])
+      jasper_file += ".jasper" unless jasper_file =~ /.jasper$/
+    else
+      jasper_file = File.join("app/views",params[:controller],params[:action]) + ".jasper"
+    end
+    if arg[:resource].size > 0 && arg[:resource][0].kind_of?(Hash)
+      resource = JasperSourceBuilder.new(arg[:resource], model_name, record_name)
+    else
+      resource = arg[:resource]
+    end
+    options = arg[:options] || {}
+    send_data JasperRails::Jasper::Rails::render_pdf(jasper_file, resource, params, options), :type => Mime::PDF
+  end
+end
